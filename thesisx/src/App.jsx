5078ae0102753cca3dff8920ceb3b568ddb56ba8
NotSoLucky <88020849+zakariaibnerafiq@users.noreply.github.com>
2024-12-02 12:04:05 +0600

    dark mode sign in

--- thesisx/src/App.jsx
+++ thesisx/src/App.jsx
@@ -7,19 +7,19 @@ function App() {
     })
     return (
         <>
-            <div className="h-screen bg-[hsl(0,0,95%)] flex items-center justify-center">
-                <div className="bg-[hsl(0,0,100%)] p-[50px] shadow-md rounded-xl max-w-[900px] m-5">
+            <div className="h-screen bg-[hsl(0,0,95%)] dark:bg-[hsl(0,0,5%)] flex items-center justify-center">
+                <div className="bg-[hsl(0,0,100%)] dark:bg-[hsl(0,0,0)] p-[50px] shadow-md rounded-xl max-w-[900px] m-5 text-black dark:text-white">
                     <div className="lg:grid lg:grid-cols-2 gap-7 ">
                         <LeftBanner />
                         <div>
                             <form action="">
                                 <div className='flex flex-col justify-center gap-2'>
                                     <label htmlFor="email">Email</label>
-                                    <input type="email" className='border outline-1 border-[hsl(0,0,30%)] rounded-lg py-2 px-4'/>
+                                    <input type="email" className='border outline-1 border-[hsl(0,0,30%)] dark:border-[hsl(0,0,70%)] rounded-lg py-2 px-4 bg-transparent' placeholder='Email'/>
                                 </div>
                                 <div className='flex flex-col justify-center gap-2 mt-3'>
                                     <label htmlFor="text">Password</label>
-                                    <input type="password" className='border outline-1 border-[hsl(0,0,30%)] rounded-lg py-2 px-4'/>
+                                    <input type="password" className='border outline-1 border-[hsl(0,0,30%)] dark:border-[hsl(0,0,70%)] rounded-lg py-2 px-4 bg-transparent' placeholder='password'/>
                                 </div>
 
                                 <div className='mt-4 flex flex-col'>
@@ -28,9 +28,9 @@ function App() {
                                     </button>
                                 </div> 
                                 <div className='flex justify-center items-center gap-1 mt-3'>
-                                    <div className='h-[1px] w-full bg-[hsl(0,0,30%)]'></div>
-                                    <p className="text-nowrap text-[hsl(0,0,30%)]">Don't have an account?</p>
-                                    <div className='h-[1px] w-full bg-[hsl(0,0,30%)]'></div>
+                                    <div className='h-[1px] w-full bg-[hsl(0,0,30%)] dark:bg-[hsl(0,0,70%)]'></div>
+                                    <p className="text-nowrap text-[hsl(0,0,30%)] dark:text-[hsl(0,0,70%)]">Don't have an account?</p>
+                                    <div className='h-[1px] w-full bg-[hsl(0,0,30%)] dark:bg-[hsl(0,0,70%)]'></div>
                                 </div>
                                 <div className='mt-4 flex flex-col'>
                                     <button className='px-6 py-2 bg-blue-600 text-white rounded-lg'>
@@ -53,7 +53,7 @@ function LeftBanner() {
                 <h1 className='font-bold text-[36px]'>
                     thesis X
                 </h1>
-                <p className='text-[14px] text-[hsl(0,0,30%)]'>
+                <p className='text-[14px] text-[hsl(0,0,30%)] dark:text-[hsl(0,0,70%)]'>
                     Empowering collaboration between students and faculty
                 </p>
             </div>
