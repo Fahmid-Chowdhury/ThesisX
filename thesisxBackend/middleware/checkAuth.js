3413f99047ef8d1294b3acfea9a87079e4bcaf14
Fahmid Chowdhury <fahmidoct26@gmail.com>
2024-12-06 02:52:17 +0600

    Update checkAuth.js

--- thesisxBackend/middleware/checkAuth.js
+++ thesisxBackend/middleware/checkAuth.js
@@ -1,19 +1,17 @@
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 
-function checkAuth(req, res, next) {
+const checkAuth = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
         const decodedToken = jwt.verify(token, process.env.JWT_KEY);
         req.userData = decodedToken;
         next();
     } catch (error) {
-        return res.status(401).json({
+        res.status(401).json({
             message: 'Auth failed',
-            error: error
+            error
         });
     }
-}
-
-module.exports = {
-    checkAuth
 };
+
+export { checkAuth };
