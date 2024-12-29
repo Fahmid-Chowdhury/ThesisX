3966f2cd3cc8137a8e81e691ac76740e09eac6cf
NotSoLucky <88020849+zakariaibnerafiq@users.noreply.github.com>
2024-12-29 17:04:22 +0600

    thesis stream sidebar

--- thesisx/src/Pages/Thesis/ThesisPage/ThesisStream/ThesisStream.jsx
+++ thesisx/src/Pages/Thesis/ThesisPage/ThesisStream/ThesisStream.jsx
@@ -9,9 +9,26 @@ const ThesisStream = () => {
 
     return (
         <ThesisStreamProvider roomId={id}>
-            <div className="flex flex-col gap-4 mt-3">
-                <PostForm />
-                <PostList />
+            <div className='flex justify-between w-full gap-4'>
+                <div className="flex flex-col gap-4 mt-3 w-full">
+                    <PostForm />
+                    <PostList />
+                </div>
+                <div className='w-96 md:block hidden'>
+                    <div className='bg-[hsl(0,0,100)] dark:bg-black mt-3  border border-[hsl(0,0%,80%)] dark:border-[hsl(0,0%,20%)] p-4 rounded-lg'>
+                        <p>Upcoming submissions</p>
+                        <div className='py-10 opacity-55'>
+                            No submissions due
+                        </div>
+                        <div className='flex justify-end items-center'>
+                            <button className='text-themeColDark dark:text-themeColLight p-2'>
+                                View all
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
         </ThesisStreamProvider>
     );
